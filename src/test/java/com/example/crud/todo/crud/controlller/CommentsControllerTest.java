fef99c07b6d6b6302a6378addcbf0f2763ab463f
Txema Lanchazo <txelan84@gmail.com>
2023-04-30 01:36:58 +0200

    Test de las peticiones correctas terminado

--- src/test/java/com/example/crud/todo/crud/controlller/CommentsControllerTest.java
+++ src/test/java/com/example/crud/todo/crud/controlller/CommentsControllerTest.java
@@ -1,20 +1,21 @@
 package com.example.crud.todo.crud.controlller;
 
+import static org.mockito.Answers.CALLS_REAL_METHODS;
 import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -23,6 +24,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.example.crud.todo.crud.controller.CommentsController;
 import com.example.crud.todo.crud.dto.CommentsDTO;
 import com.example.crud.todo.crud.dto.PublicationDTO;
 import com.example.crud.todo.crud.entities.Comments;
@@ -77,49 +79,82 @@ public class CommentsControllerTest {
     }
     
     @Test
-    void testCommentsListByPublicationId() {
-
-    }
-
-    @Test
-    void testDeleteComment() {
-
-    }
-
-    @Test
-    void testGetCommentByIdFromPublicationId() throws Exception {
-        Long publicationId = 1L;
-
+    void testCommentsListByPublicationId() throws Exception{
+        long publicationId = 1L;
         when(commentsService.getCommentsByPublicationId(publicationId)).thenReturn(Arrays.asList(commentsDTO));
 
-        ResultActions response = mockMvc.perform(get("/publication/{publicationId}/comments",publicationId)
+        ResultActions response = mockMvc.perform(get("/api/publications/{publicationId}/comments",publicationId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(commentsDTO)));
 
         response.andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()",CoreMatchers.is(Arrays.asList(commentsDTO).size())));
+                .andExpect(jsonPath("$.size()",is(Arrays.asList(commentsDTO).size())));
+
+    }
+
+    @Test
+    void testDeleteComment() throws Exception{
+        long commentId = 1L;
+        long publicationId = 1L;
+
+        willDoNothing().given(commentsService).deleteComment(publicationId,commentId);
+
+        ResultActions response = mockMvc.perform(delete("/api/publications/{publicationId}/comments/{commentId}",publicationId,commentId)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    void testGetCommentByIdFromPublicationId() throws Exception {
+        long publicationId = 1L;
+
+        when(commentsService.getCommentsByPublicationId(publicationId)).thenReturn(Arrays.asList(commentsDTO));
+
+        ResultActions response = mockMvc.perform(get("/api/publications/{publicationId}/comments",publicationId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(commentsDTO)));
+
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()",is(Arrays.asList(commentsDTO).size())));
     }
 
     @Test
     void testSaveComment() throws Exception{
-        Long publicationId = 1L;
-        given(commentsService.createComment(publicationId, commentsDTO)).willReturn(commentsDTO);
+        long publicationId = 1L;
+        when(commentsService.createComment(publicationId, commentsDTO)).thenReturn(commentsDTO);
 
-        ResultActions response = mockMvc.perform(post("/publication/{publicationId}/comments", publicationId)
+        ResultActions response = mockMvc.perform(post("/api/publications/{publicationId}/comments", publicationId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(commentsDTO)));
+
+        response.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name",is(commentsDTO.getName())))
+                .andExpect(jsonPath("$.email",is(commentsDTO.getEmail())))
+                .andExpect(jsonPath("$.body",is(commentsDTO.getBody())));
+
+    }
+
+    @Test
+    void testUpdateComment() throws Exception{
+        Long publicationId = 1L;
+        Long commentId = 1L;
+
+        when(commentsService.updateComment(publicationId, commentId, commentsDTO)).thenReturn(commentsDTO);
+
+        ResultActions response = mockMvc.perform(put("/api/publications/{publicationId}/comments/{commentId}",publicationId,commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(commentsDTO)));
+              
 
         response.andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name",is(comments.getName())))
                 .andExpect(jsonPath("$.email",is(comments.getEmail())))
                 .andExpect(jsonPath("$.body",is(comments.getBody())));
-
-    }
-
-    @Test
-    void testUpdateComment() {
-
     }
 }
