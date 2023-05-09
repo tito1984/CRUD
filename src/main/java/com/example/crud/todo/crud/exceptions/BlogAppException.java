9d13a7123d274d59ef36427dd94c755bd499183c
Txema Lanchazo <txelan84@gmail.com>
2023-05-09 15:33:32 +0200

    Ultimos arreglos

--- src/main/java/com/example/crud/todo/crud/exceptions/BlogAppException.java
+++ src/main/java/com/example/crud/todo/crud/exceptions/BlogAppException.java
@@ -1,7 +1,10 @@
 package com.example.crud.todo.crud.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class BlogAppException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
